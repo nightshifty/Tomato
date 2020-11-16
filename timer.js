ca130b62ce8b6a0bff4efe40e567f44eea8d634c
Yasin Ilyas <yasin.ilyas@stud.h-da.de>
2020-11-16 15:51:49 +0100

    first working timer clientside script

---
+++ timer.js
@@ -0,0 +1,43 @@
+let playBTn = $("#playbtn");
+let finishTime;
+let pomodoroTime = 25;
+let timerRunning;
+
+playBTn.click(function(){
+    togglePlayBtn();
+});
+
+function togglePlayBtn(){
+    "use strict";
+    if(playBTn.text() === "START") {
+        startTimer();
+        playBTn.text("STOP");
+    }
+    else {
+        stopTimer();
+        playBTn.text("START");
+    }
+}
+
+function startTimer(){
+    "use strict";
+    let millisecondsToAdd = pomodoroTime*60*1000;
+    finishTime = Date.now()+millisecondsToAdd;
+    timerRunning = setInterval(function(){ timer(); }, 1000);
+}
+
+function stopTimer(){
+    "use strict";
+    clearInterval(timerRunning);
+}
+
+function timer(){
+    "use strict";
+    console.log("timer started...")
+    if(finishTime>Date.now()){
+        let remainingseconds = Math.round((finishTime-Date.now()) / 1000);
+        let remainingminutes = Math.floor(remainingseconds / 60);
+        remainingseconds-= remainingminutes*60;
+        $("#timer").text(remainingminutes+":"+remainingseconds);
+    }
+}
