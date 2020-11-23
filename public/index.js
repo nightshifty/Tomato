8b483553f8492921a42f5722af35325a836f38b7
larissablachfelder <44058453+larissablachfelder@users.noreply.github.com>
2020-11-23 11:07:16 +0000

    try fix login and validation

--- public/index.js
+++ public/index.js
@@ -35,12 +35,51 @@ $( document ).ready(function() {
 		if(jqXHR.status === 403){
 			//user is not authorized
 			console.log("user is not authorized");
+			$("#todos").hide();
 		}else{
 			//user is authorized @ Larissa: Call your function with Ajax Magic from here
 			console.log("user is authorized");
+			$("#todos").show();
 		}
-    console.log(jqXHR.status);
+	console.log(jqXHR.status);
+	
+//validation of forms
+// Initialize form validation on the registration form.
+	// It has the name attribute "registration"
+	$("form[name='registration']").validate({
+		// Specify validation rules
+		rules: {
+		
+		  username: "required",
+		  password: {
+			required: true,
+			minlength: 5
+		  }
+		},
+		// Specify validation error messages
+		messages: {
+		  username: "Please enter your firstname",
+		  password: {
+			required: "Please provide a password",
+			minlength: "Your password must be at least 5 characters long"
+		  },
+		},
+		// Make sure the form is submitted to the destination defined
+		// in the "action" attribute of the form when valid
+		submitHandler: function(form) {
+		  form.submit();
+		}
+	  });
+
+
 });
+
+//validation of forms 
+
+
+
+
+
 });
 
 $(function() {
