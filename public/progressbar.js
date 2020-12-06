8bf9e93066d92246c0d1be3b2a35764a04801a52
Yasin Ilyas <yasin.ilyas@stud.h-da.de>
2020-12-06 23:52:01 +0100

    inform about external code

--- public/progressbar.js
+++ public/progressbar.js
@@ -1,5 +1,12 @@
+/**
+ * Actually a part of the timer, excluded to make clear it´s an (modified) external code snippet
+ */
 
-
+/**
+ * modified EXTERNAL CODE SNIPPET
+ * Credit: Mateusz Rybczonec
+ * https://css-tricks.com/how-to-create-an-animated-countdown-timer-with-html-css-and-javascript/
+ */
 function setCircleDasharray(percentage) {
     const circleDasharray = `${(
         percentage * 2.83
@@ -8,3 +15,4 @@ function setCircleDasharray(percentage) {
         .getElementById("base-timer-path-remaining")
         .setAttribute("stroke-dasharray", circleDasharray);
 }
+//end of modified external code snippet
